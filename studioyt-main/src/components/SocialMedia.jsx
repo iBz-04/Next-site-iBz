2ce6d8fba13726221d02dce5a238cdba75e40d30
Ibrahim Rayamah <issakaibrahimrayamah@gmail.com>
2024-05-29 00:51:16 +0300

    first commit

---
+++ studioyt-main/src/components/SocialMedia.jsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import clsx from "clsx";
+import {
+  BsFacebook,
+  BsTwitter,
+  BsGithub,
+  BsYoutube,
+  BsLinkedin,
+} from "react-icons/bs";
+
+export const SocialMediaProfiles = [
+  {
+    title: "Youtube",
+    href: "https://www.youtube.com/",
+    icon: BsYoutube,
+  },
+  { title: "GitHub", href: "https://github.com/iBz-04/", icon: BsGithub },
+  {
+    title: "Facebook",
+    href: "https://www.facebook.com/",
+    icon: BsFacebook,
+  },
+  {
+    title: "Instagram",
+    href: "https://www.linkedin.com/in/",
+    icon: BsLinkedin,
+  },
+  {
+    title: "Twitter",
+    href: "https://twitter.com/ibrahh__ib/",
+    icon: BsTwitter,
+  },
+];
+
+const SocialMedia = ({ className, invert = false }) => {
+  return (
+    <ul
+      role="list"
+      className={clsx(
+        "flex gap-x-10",
+        invert ? "text-white" : "text-neutral-950",
+        className
+      )}
+    >
+      {SocialMediaProfiles.map((item) => (
+        <li key={item.title}>
+          <Link
+            href={item.href}
+            aria-label={item.title}
+            className={clsx(
+              "transition",
+              invert ? "hover:text-neutral-200" : "hover:text-neutral-700"
+            )}
+          >
+            <item.icon className="h-6 w-6 fill-current" />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default SocialMedia;
